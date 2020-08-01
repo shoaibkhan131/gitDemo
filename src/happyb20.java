0f8c2fcd9fc583bb8dbf9da4c22eeb8051079076
shoaibkhan131 <68969191+shoaibkhan131@users.noreply.github.com>
2020-08-01 16:35:38 -0400

    Update happyb20.java

--- src/happyb20.java
+++ src/happyb20.java
@@ -1,6 +1,12 @@
 public class happyb20 {
     public static void main(String[] args) {
         System.out.println("b20 class mates are happy to study automation");
+        
+        
+        
+        
+        
+        System.out.println("  make changes");
     }
 }
 
